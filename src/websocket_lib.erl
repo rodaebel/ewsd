8f6b870d70ff5383465faeba1bd15836793b6c45
Tobias <tobias@LittleGeek.local>
2011-02-14 23:30:43 +0100

    Added tests.

--- src/websocket_lib.erl
+++ src/websocket_lib.erl
@@ -47,6 +47,9 @@ get_header_value(String, [H|T]) ->
         _Pos -> string:substr(H, string:len(String) + 3)
     end.
 
+get_header_value_test_() ->
+    ?_assertEqual("test", get_header_value("Test", ["Foo: foo", "Test: test"])).
+
 %% @private
 %% @doc Calcualte the handshake challenge.
 %% @spec calc_challenge(Key1, Key2, Key3) -> Result
@@ -60,12 +63,26 @@ calc_challenge(Key1, Key2, Key3) ->
                              Key3/binary>>),
     Challenge.
 
+calc_challenge_test_() ->
+    ?_assertEqual(<<249,214,215,98,131,120,84,118,243,69,68,91,56,235,101,215>>,
+                  calc_challenge("89z sdhf 9sf", "fh siu is7f7t", <<"foo">>)).
+
 %% @private
 %% @doc Concatenates digits 0-9 of a string.
 %% @spec concat_digits(String) -> List
 concat_digits(String) -> [A || A <- String, A =< 57, A >= 48].
 
+concat_digits_test_() ->
+    [?_assertEqual([], concat_digits("Foo bar")),
+     ?_assertEqual("82", concat_digits("hiu sd8 2  hfs")),
+     ?_assertEqual("042007", concat_digits("0Foo42 bar007 test"))].
+
 %% @private
 %% @doc Counts number of spaces in a string.
 %% @spec count_spaces(String) -> Integer
 count_spaces(String) -> string:len([ A || A <- String, A =:= 32]).
+
+count_spaces_test_() ->
+    [?_assertEqual(0, count_spaces("Foobar")),
+     ?_assertEqual(1, count_spaces("Foo bar")),
+     ?_assertEqual(2, count_spaces("Foo bar test"))].
