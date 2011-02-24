bcf74fea3539a21d050126a0f5ed634f70b06e04
Tobias <tobias@LittleGeek.local>
2011-02-24 22:15:24 +0100

    Improved broadcast handler.

--- src/websocket_broadcast.erl
+++ src/websocket_broadcast.erl
@@ -18,15 +18,16 @@ init_handler() ->
 %% @spec handle_message({Type, Socket, Data}) -> any()
 handle_message({handshake, Socket, Data}) ->
     {ok, Response, Path} = websocket_lib:process_handshake(Data),
-    ets:insert_new(clients, {Socket}),
+    ets:insert_new(clients, {Socket, Path}),
     gen_tcp:send(Socket, Response),
     error_logger:info_msg("~p Socket connected (~s)~n", [self(), Path]);
 handle_message({message, _Socket, Data}) ->
     Frame = [0, Data, 255],
-    ets:foldl(fun({S}, _Acc) -> gen_tcp:send(S, Frame) end, notused, clients).
+    ets:foldl(fun({S, _}, _Acc) -> gen_tcp:send(S, Frame) end,
+              notused, clients).
 
 %% @doc Handles closed Web Socket.
 %% @spec handle_close(Socket) -> any()
 handle_close(Socket) ->
-    ets:delete_object(clients, {Socket}),
+    ets:match_delete(clients, {Socket, '_'}),
     error_logger:info_msg("~p Socket closed~n", [self()]).
